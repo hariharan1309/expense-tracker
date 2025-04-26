5235b8cfeea755877b5ceabf6884af606621fe08
hariharan1309 <63937651+hariharan1309@users.noreply.github.com>
2025-04-26 11:10:22 +0530

    router dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+
 function App() {
-  return <>Expense Tracker</>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
