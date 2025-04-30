a2d82dee9d9ba59fb18e22405b4efbbb27a91a25
hariharan1309 <63937651+hariharan1309@users.noreply.github.com>
2025-04-30 08:14:54 +0530

    Expense Schema

---
+++ backend/utils/models/Expense.ts
@@ -0,0 +1,48 @@
+import mongoose, { Schema } from "mongoose";
+
+const ExpenseSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Please provide a title"],
+      trim: true,
+    },
+    amount: {
+      type: Number,
+      required: [true, "Please provide an amount"],
+    },
+    category: {
+      type: String,
+      required: [true, "Please provide a category"],
+      enum: [
+        "food",
+        "transportation",
+        "entertainment",
+        "utilities",
+        "housing",
+        "healthcare",
+        "education",
+        "other",
+      ],
+    },
+    date: {
+      type: Date,
+      required: [true, "Please provide a date"],
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true, // adding additional expense details.
+  }
+);
+
+const Expesnse = mongoose.model("Expense", ExpenseSchema);
+export default Expesnse;
